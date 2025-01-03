ba484d1b20d690707cd135e72214f4efaf4be2d9
Hidan12 <marcarfe12@gmail.com>
2025-01-02 20:36:49 -0500

    fixed footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 
 
 const FormContact = ()=>{
@@ -8,15 +9,47 @@ const FormContact = ()=>{
             <textarea className="rounded-md w-full" name="" placeholder="message (Optional)" id="">
 
             </textarea>
-            <button className="text-white">
+            <button className="text-white p-3 rounded-md bg-blue-600 hover:bg-blue-900">
                 Send
             </button>
         </form>
     )
 }
 
+const MobileFooter = ()=>{
+    return(
+        <footer className="flex flex-col justify-center w-full bg-black bg-gradient-to-r from-[#434343] to-black">
+            <div id="contact" className="flex justify-center items-center pt-4">
+                <FormContact/>
+            </div>
+            <div>
+                <div className="footer-bottom text-center py-5 text-[#ccc]">
+                    2024 Designed by <a href="https://github.com/Hidan12" target="_blank" className="hover:text-blue-600">Martin Vidan</a>
+                </div>
+            </div>
+
+        </footer>
+    )
+}
 
 const Footer = () => {
+    const [windowSize, setWindowSize] = useState({width: window.innerWidth})
+        useEffect(() => {
+            const handleResize = () => {
+              setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+              });
+            };
+            window.addEventListener('resize', handleResize);
+            handleResize();
+        }, [])
+
+        if (windowSize.width < 600) {
+            return(
+                <MobileFooter/>
+            )
+        }
     return (
       <footer className="flex flex-col bg-black bg-gradient-to-r from-[#434343] to-black">
         <div className="w-full grid grid-cols-3 pt-4">
@@ -41,7 +74,7 @@ const Footer = () => {
         </div>
         <div className="footer-bottom text-center py-5 text-[#ccc]">
         2024 Designed by <a href="https://github.com/Hidan12" target="_blank" className="hover:text-blue-600">Martin Vidan</a>
-      </div>       
+      </div>
       </footer>
     );
   };
